f04adbc84599042a156fddd3455f2d6d17575366
Diego <73961367+deviego@users.noreply.github.com>
2023-02-17 17:50:08 -0300

    fist commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import styles from "./Header.module.css";
+
+export const Header = () => {
+  return (
+    <>
+      <header>
+        <strong className={styles.header}>Feed Ignite</strong>
+      </header>
+    </>
+  );
+};
